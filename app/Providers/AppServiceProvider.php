0767cd0d3798bfd511c03eb49a4dc62e51e6bf73
Andy Ford <andyford993@gmail.com>
2022-05-02 15:47:00 +0100

    Add FlightInformationRegion resource

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Http\Resources\FlightInformationRegionResource;
 use App\Http\Resources\FlowMeasureResource;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,5 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         FlowMeasureResource::withoutWrapping();
+        FlightInformationRegionResource::withoutWrapping();
     }
 }
