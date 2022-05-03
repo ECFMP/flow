793ca58bac5f55a53c31a47d6b8b4fd00e32bd1f
Andy Ford <andyford993@gmail.com>
2022-05-03 20:49:06 +0100

    Add model relations

--- app/Models/DiscordTag.php
+++ app/Models/DiscordTag.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class DiscordTag extends Model
 {
@@ -10,4 +11,9 @@ class DiscordTag extends Model
         'tag',
         'description',
     ];
+
+    public function flightInformationRegions(): BelongsToMany
+    {
+        return $this->belongsToMany(FlightInformationRegion::class);
+    }
 }
