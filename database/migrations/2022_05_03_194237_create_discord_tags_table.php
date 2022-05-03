0cb70b9e95a6a6bf60ac9a959187d1dfb290e20b
Andy Ford <andyford993@gmail.com>
2022-05-03 20:43:39 +0100

    Create discord tags table

---
+++ database/migrations/2022_05_03_194237_create_discord_tags_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discord_tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('tag')->unique()->comment('The tag to use');
+            $table->string('description')->comment('What the tag is for / who it is targeted at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('discord_tags');
+    }
+};
