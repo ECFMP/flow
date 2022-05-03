68127caee8de826dc7bc3adba85591cde8a7def8
Dave Roverts <me@daveroverts.nl>
2022-05-03 22:19:37 +0200

    feat: add discordtag relation to fir

--- app/Filament/Resources/FlightInformationRegionResource.php
+++ app/Filament/Resources/FlightInformationRegionResource.php
@@ -72,6 +72,7 @@ class FlightInformationRegionResource extends Resource
     {
         return [
             RelationManagers\UsersRelationManager::class,
+            RelationManagers\DiscordTagsRelationManager::class,
         ];
     }
 
