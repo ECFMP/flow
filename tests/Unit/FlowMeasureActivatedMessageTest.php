06e188ceb30b3f7c26475274c341f9b9506e8e06
Andy Ford <andyford993@gmail.com>
2022-05-06 20:08:00 +0100

    Activated message test

---
+++ tests/Unit/FlowMeasureActivatedMessageTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Discord\FlowMeasure\Message\FlowMeasureActivatedMessage;
+use App\Discord\Message\Content\ContentInterface;
+use Tests\TestCase;
+
+class FlowMeasureActivatedMessageTest extends TestCase
+{
+    private function getContent(): ContentInterface
+    {
+        return new class implements ContentInterface {
+            public function toString(): string
+            {
+                return 'ohai';
+            }
+        };
+    }
+
+    public function testItReturnsMessage()
+    {
+        $this->assertEquals(
+            "Flow Measure Activated: \n\nohai",
+            (new FlowMeasureActivatedMessage($this->getContent()))->content()
+        );
+    }
+}
