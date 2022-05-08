5ccc5a50a1c64893d48756b5ca4f27b45dbcc093
Andy Ford <andyford993@gmail.com>
2022-05-08 16:58:49 +0100

    Redirect homepage to filament login

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return to_route('filament.auth.login');
 });
 
 Route::middleware(['guest'])->group(function () {
