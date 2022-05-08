72514c1402ac1680162d040931b3fbf197f489c9
Andy Ford <andyford993@gmail.com>
2022-05-08 16:31:59 +0100

    Use RefreshDatabase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,23 @@
 
 namespace Tests;
 
+use DB;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, RefreshDatabase;
+
+    public function beforeRefreshingDatabase()
+    {
+        DB::table('discord_notifications')->delete();
+        DB::table('flow_measures')->delete();
+        DB::table('events')->delete();
+        DB::table('flight_information_regions')->delete();
+        DB::table('users')->delete();
+        DB::table('airport_groups')->delete();
+        DB::table('airports')->delete();
+        DB::table('discord_tags')->delete();
+    }
 }
