ca0d2b94677ec2aa5d66381a726b050ece96e06e
Andy Ford <andyford993@gmail.com>
2022-05-22 19:26:06 +0100

    Send new message types in command

--- app/Console/Commands/SendDiscordNotifications.php
+++ app/Console/Commands/SendDiscordNotifications.php
@@ -34,6 +34,7 @@ class SendDiscordNotifications extends Command
         }
 
         $this->info('Sending discord notifications');
+        $discordMessageService->sendMeasureApproachingDiscordNotifications();
         $discordMessageService->sendMeasureActivatedDiscordNotifications();
         $discordMessageService->sendMeasureWithdrawnDiscordNotifications();
         $discordMessageService->sendMeasureExpiredDiscordNotifications();
