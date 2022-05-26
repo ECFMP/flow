8270e0e2ad732255d59eab0daedeb2e668186c37
Dave Roverts <me@daveroverts.nl>
2022-05-26 14:02:22 +0200

    chore: add strong to time if date is also shown

--- resources/views/filament/tables/columns/flow-measure/end-time.blade.php
+++ resources/views/filament/tables/columns/flow-measure/end-time.blade.php
@@ -2,6 +2,6 @@
     @if ($getRecord->start_time->isSameDay($getRecord->end_time))
         {{ $getState->format('H:i\z') }}
     @else
-        {{ $getState->format('M j, Y H:i\z') }}
+        {{ $getState->format('M j, Y') }} <strong>{{ $getState->format('H:i\z') }}</strong>
     @endif
 </div>
