78285b2331114cdbba5ae67b26e67de4d842f6c4
Andy <andyford993@gmail.com>
2022-06-09 12:26:00 +0100

    Remove enum method

--- app/Enums/FilterType.php
+++ app/Enums/FilterType.php
@@ -13,18 +13,4 @@ enum FilterType: string
     case MEMBER_EVENT = 'member_event';
     case MEMBER_NOT_EVENT = 'member_not_event';
     case RANGE_TO_DESTINATION = 'range_to_destination';
-
-    public function getShortName(): string
-    {
-        return match ($this) {
-            self::DEPARTURE_AIRPORTS => 'DEPA',
-            self::ARRIVAL_AIRPORTS => 'DEST',
-            self::WAYPOINT => 'VIA WPT',
-            self::LEVEL_ABOVE => 'LVL ABV',
-            self::LEVEL_BELOW => 'LVL BLW',
-            self::LEVEL => 'LVL',
-            self::MEMBER_EVENT => 'EVENT',
-            self::MEMBER_NOT_EVENT => 'NON EVENT',
-        };
-    }
 }
