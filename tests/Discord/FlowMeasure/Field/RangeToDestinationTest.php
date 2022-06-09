45dfe6f97ada01a00981859fbd44a639dda9ca6c
Andy <andyford993@gmail.com>
2022-06-09 12:39:46 +0100

    Handle in discord

---
+++ tests/Discord/FlowMeasure/Field/RangeToDestinationTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Discord\FlowMeasure\Field;
+
+use App\Discord\FlowMeasure\Field\Filters\RangeToDestination;
+use Tests\TestCase;
+
+class RangeToDestinationTest extends TestCase
+{
+    private function getField(): RangeToDestination
+    {
+        return new RangeToDestination(
+            [
+                'type' => 'range_to_destination',
+                'value' => ["250"],
+            ]
+        );
+    }
+
+    public function testItHasAName()
+    {
+        $this->assertEquals(
+            'Range To Destination (NM)',
+            $this->getField()->name()
+        );
+    }
+
+    public function testItHasARange()
+    {
+        $this->assertEquals(
+            '250',
+            $this->getField()->value()
+        );
+    }
+}
