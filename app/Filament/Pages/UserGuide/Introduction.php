0d7879c19a6c6370baffb433f89460152e576659
Dave Roverts <me@daveroverts.nl>
2022-06-27 21:43:04 +0200

    feat: add md parser and add initial pages

---
+++ app/Filament/Pages/UserGuide/Introduction.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Pages\UserGuide;
+
+use Filament\Pages\Page;
+use Spatie\LaravelMarkdown\MarkdownRenderer;
+
+class Introduction extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.user-guide.markdown-parser';
+
+    protected static ?string $navigationGroup = 'User Guide';
+
+    protected static ?int $navigationSort = 1;
+
+    protected function getViewData(): array
+    {
+        return [
+            'md' => app(MarkdownRenderer::class)->toHtml(file_get_contents(base_path('docs/Introduction.md'))),
+        ];
+    }
+}
