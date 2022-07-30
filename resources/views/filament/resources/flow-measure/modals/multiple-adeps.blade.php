db1482ca2988ba31f661ab46209fb3adde4f0e7e
Dave Roverts <me@daveroverts.nl>
2022-07-30 13:36:18 +0200

    feat(CreateFlowMeasure): show warning if using multiple adeps
    Closes #180

---
+++ resources/views/filament/resources/flow-measure/modals/multiple-adeps.blade.php
@@ -0,0 +1,11 @@
+<div>
+    <p>You have submitted a departure flow restriction applying to multiple airports.
+        This is <strong>usually incorrect</strong> as it requires the flow measure to be coordinated across multiple
+        airports. If you have arranged/agreed this with a flow manager to coordinate on the network, you may proceed.
+    </p>
+
+    <br />
+
+    <p>The more likely situation is that you are trying to apply the departure flow restriction separately to multiple
+        airports. For this you must issue separate flow measures</p>
+</div>
