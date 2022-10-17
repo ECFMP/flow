67703b22712d092a9004b42fad0ca297eb249a1b
Andy <andyford993@gmail.com>
2022-10-17 20:27:25 +0100

    test: fix test

--- resources/views/filament/tables/columns/flow-measure/end-time.blade.php
+++ resources/views/filament/tables/columns/flow-measure/end-time.blade.php
@@ -1,7 +1,7 @@
 <div>
-    @if ($getRecord->start_time->isSameDay($getRecord->end_time))
-        {{ $getState->format('H:i\z') }}
+    @if ($getRecord()->start_time->isSameDay($getRecord()->end_time))
+        {{ $getState()->format('H:i\z') }}
     @else
-        {{ $getState->format('M j, Y') }} <strong>{{ $getState->format('H:i\z') }}</strong>
+        {{ $getState()->format('M j, Y') }} <strong>{{ $getState()->format('H:i\z') }}</strong>
     @endif
 </div>
