970a8ee5250dfce32bb759d1ff3d920c2107141d
Andy Ford <andyford993@gmail.com>
2022-11-26 17:32:31 +0000

    fix: select width

--- resources/views/filament/pages/airport-traffic.blade.php
+++ resources/views/filament/pages/airport-traffic.blade.php
@@ -1,8 +1,10 @@
 <x-filament::page>
-    @livewire('airport-search')
+    <div style="max-width: 25%">
+        @livewire('airport-search')
+    </div>
     @if ($airport && $airport['latitude'] && $airport['longitude'])
-        @livewire('airport-overview', ['airportId' => $airport['id']])
-        @livewire('airport-inbounds', ['airportId' => $airport['id']])
+    @livewire('airport-overview', ['airportId' => $airport['id']])
+    @livewire('airport-inbounds', ['airportId' => $airport['id']])
     @elseif ($airport)
     <div>Cannot display arrival information for this airport. Please ensure that the airports latitude and longitude are
         configured.</div>
--- resources/views/livewire/airport-search.blade.php
+++ resources/views/livewire/airport-search.blade.php
@@ -1,5 +1,5 @@
 <div class="relative">
-    <div class="space-y-2 flex-1">
+    <div class="space-y-2">
         <div class="flex items-center justify-between space-x-2 rtl:space-x-reverse">
             <label class="filament-forms-field-wrapper-label inline-flex items-center space-x-3 rtl:space-x-reverse"
                 for="airportSelection">
@@ -8,19 +8,18 @@
                 </span>
             </label>
         </div>
-        <div class="filament-forms-select-component flex items-center space-x-1 rtl:space-x-reverse group">
-            <input type="text" id="airportSelection"
+        <div class="filament-forms-select-component flex items-center rtl:space-x-reverse group">
+            <input type="text" id="airportSelection" style="width: 100%"
                 class="block transition duration-75 rounded-lg shadow-sm focus:border-primary-500 focus:ring-1 focus:ring-inset focus:ring-primary-500 disabled:opacity-70 dark:bg-gray-700 dark:text-white dark:focus:border-primary-500 border-gray-300 dark:border-gray-600"
                 placeholder="Search Airports..." wire:model="query" wire:keydown.escape="resetSearch"
                 wire:keydown.tab="resetSearch" wire:keydown.arrow-up="decrementHighlight"
-                wire:keydown.arrow-down="incrementHighlight" wire:keydown.enter="selectAirport"
-                wire:change="resetSearch" />
+                wire:keydown.arrow-down="incrementHighlight" wire:keydown.enter="selectAirport" />
 
             @if(!empty($query))
             <div class="choices__list choices__list--dropdown is-active block">
                 @if(!empty($airports))
                 @foreach($airports as $i => $airport)
-                <div wire:mouseover="setHighlight({{$i}})" , wire:click="selectAirport"
+                <div wire:mouseover="setHighlight({{$i}})" , wire:click="selectAirport" style="width: 100%"
                     class="choices__item choices__item--choice choices__item--selectable  {{ $highlightIndex === $i ? 'is-highlighted' : '' }}">
                     {{$airport['icao_code'] }}
                 </div>
