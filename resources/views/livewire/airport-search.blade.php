743327d5e69ebb56e03474d622c0666838a2d605
Andy Ford <andyford993@gmail.com>
2022-11-26 17:49:17 +0000

    fix: clicking out

--- resources/views/livewire/airport-search.blade.php
+++ resources/views/livewire/airport-search.blade.php
@@ -9,18 +9,18 @@
             </label>
         </div>
         <div class="filament-forms-select-component flex items-center rtl:space-x-reverse group">
-            <input type="text" id="airportSelection" style="width: 100%"
+            <input type="search" id="airportSelection" style="width: 100%"
                 class="block transition duration-75 rounded-lg shadow-sm focus:border-primary-500 focus:ring-1 focus:ring-inset focus:ring-primary-500 disabled:opacity-70 dark:bg-gray-700 dark:text-white dark:focus:border-primary-500 border-gray-300 dark:border-gray-600"
                 placeholder="Search Airports..." wire:model="query" wire:keydown.escape="resetSearch"
                 wire:keydown.tab="resetSearch" wire:keydown.arrow-up="decrementHighlight"
-                wire:keydown.arrow-down="incrementHighlight" wire:keydown.enter="selectAirport" />
+                wire:keydown.arrow-down="incrementHighlight" wire:keydown.enter="selectAirport"/>
 
             @if(!empty($query))
-            <div class="choices__list choices__list--dropdown is-active block">
+            <div class="choices__list choices__list--dropdown is-active block listbox" @click.outside="$wire.resetSearch()">
                 @if(!empty($airports))
                 @foreach($airports as $i => $airport)
                 <div wire:mouseover="setHighlight({{$i}})" , wire:click="selectAirport" style="width: 100%"
-                    class="choices__item choices__item--choice choices__item--selectable  {{ $highlightIndex === $i ? 'is-highlighted' : '' }}">
+                    class="choices__item choices__item--choice choices__item--selectable {{ $highlightIndex === $i ? 'is-highlighted' : '' }}">
                     {{$airport['icao_code'] }}
                 </div>
                 @endforeach
